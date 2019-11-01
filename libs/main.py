668d7d0b578e4dba286c1dca93447694afd62b44
Sergey Petrovich <52720435+serp404@users.noreply.github.com>
2019-11-01 12:44:44 +0300

    Update main.py

--- libs/main.py
+++ libs/main.py
@@ -1,14 +1,13 @@
-import torch
 import pyro
 
 pyro.set_rng_seed(100)
 
 def weather(): 
-    cloudy = torch.distributions.Bernoulli(0.3).sample() 
+    cloudy = pyro.distributions.Bernoulli(0.3).sample() 
     cloudy = 'cloudy' if cloudy.item()  ==  1.0  else 'sunny'
     mean_temp = {'cloudy': 55.0, 'sunny': 75.0}[cloudy] 
     scale_temp = {'cloudy': 10.0, 'sunny': 15.0}[cloudy] 
-    temp = torch.distributions.Normal(mean_temp, scale_temp).rsample() 
+    temp = pyro.distributions.Normal(mean_temp, scale_temp).rsample() 
     return cloudy, temp.item()
 
 def ice_cream_sales():
