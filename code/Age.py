2edef3d0190ab9406f286698608ca0607ebd30f1
Sergey Petrovich <serg.petrovich2000@gmail.com>
2020-05-17 12:30:26 +0300

    Update Age.py

--- code/Age.py
+++ code/Age.py
@@ -3,17 +3,16 @@ import pyro.distributions as distrs
 
 pyro.set_rng_seed(100)
 
-random_noise = distrs.Normal(0.0, 0.05)  # mean = 0.0, sigma = 0.05
+random_noise = distrs.Normal(0.0, 0.15)  # mean = 0.0, sigma = 0.15
 
 class FactorAge:
     def __init__(self, age: int):
         self.age = age
     
     def get_accel(self)->float:
-        # depends on sex in some cases
         accel = 0.0
         if 17 <= self.age < 21:
-            accel = distrs.Laplace(0.22, 0.15)().item()  # mean = 0.22, sigma = 0.15
+            accel = distrs.Laplace(0.25, 0.15)().item()  # mean = 0.25, sigma = 0.15
         elif 21 <= self.age < 26:
             accel = distrs.Normal(0.15, 0.15)().item()  # mean = 0.15, sigma = 0.15
         elif 26 <= self.age < 45:
@@ -25,7 +24,7 @@ class FactorAge:
             if condition == "Normal":
                 accel = random_noise().item()
             else:
-                accel = distrs.Uniform(0.15, 0.35)().item()  # lower_bound = 0.15, upper_bound = 0.35
+                accel = distrs.Uniform(0.2, 0.35)().item()  # lower_bound = 0.2, upper_bound = 0.35
         
         return accel
 
@@ -34,7 +33,7 @@ class FactorAge:
         if 17 <= self.age < 55:
             decel = random_noise().item()
         else:
-            decel = distrs.Uniform(-0.3, 0.15)().item()  # lower_bound = -0.3, upper_bound = 0.15
+            decel = distrs.Uniform(-0.3, 0.2)().item()  # lower_bound = -0.3, upper_bound = 0.2
         
         return decel
 
@@ -43,28 +42,12 @@ class FactorAge:
         if 17 <= self.age < 45:
             sigma = random_noise().item()
         elif 45 <= self.age < 55:
-            sigma = distrs.Normal(0.1, 0.15)().item()  # mean = 0.1, sigma = 0.15
+            sigma = distrs.Normal(0.1, 0.05)().item()  # mean = 0.1, sigma = 0.05
         else:
-            sigma = distrs.Chi2(2)().item() / 10  # k = 2 -> mean = 0.1, sigma = 0.2
+            sigma = distrs.Chi2(2)().item() / 10  # k = 2 -> mean = 0.1 , sigma = 0.2
     
         return sigma
         
-    def get_speedFactor(self)->float:
-        # depends on sex in some cases
-        speedFactor = 0.0
-        if 17 <= self.age < 21:
-            speedFactor = distrs.Laplace(0.2, 0.25)().item()  # mean = 0.2, sigma = 0.25
-        elif 21 <= self.age < 65:
-            speedFactor = distrs.Normal(0.0, 0.5)().item()  # mean = 0, sigma = 0.5
-        else:
-            condition = { 0: "Slow", 1: "Fast" }[distrs.Bernoulli(0.3)().item()]  # success_prob = 0.3
-            if condition == "Slow":
-                speedFactor = distrs.Normal(0.0, 0.2)().item()  # mean = 0, sigma = 0.2
-            else:
-                speedFactor = distrs.Uniform(0.25, 0.35)().item()  # lower_bound = 0.25, upper_bound = 0.35
-        
-        return speedFactor
-
     def get_minGap(self)->float:
         minGap = 0.0
         if 17 <= self.age < 30:
@@ -74,16 +57,16 @@ class FactorAge:
         else:
             condition = { 0: "Normal", 1: "Fast"}[distrs.Bernoulli(0.4)().item()]  # success_prob = 0.4
             if condition == "Normal":
-                minGap = distrs.Uniform(-0.05, 0.15)().item()  # lower_bound = -0.05, upper_bound = 0.15
+                minGap = distrs.Uniform(0.0, 0.15)().item()  # lower_bound = 0.0, upper_bound = 0.15
             else:
-                minGap = distrs.Normal(-0.12, 0.05)().item()  # mean = -0.12, sigma = 0.05
+                minGap = distrs.Normal(-0.1, 0.05)().item()  # mean = -0.1, sigma = 0.05
     
         return minGap
 
     def get_actionStepLength(self)->float:
         actionStepLength = 0.0
         if 17 <= self.age < 45:
-            actionStepLength = distrs.Uniform(-0.25, 0.1)().item()  # lower_bound = -0.25, upper_bound = 0.1
+            actionStepLength = distrs.Uniform(-0.1, 0.1)().item()  # lower_bound = -0.1, upper_bound = 0.1
         else:
             actionStepLength = distrs.Chi2(2)().item() / 16 - 0.025  # k = 2 -> mean = 0.125, sigma = 0.125
     
@@ -97,3 +80,111 @@ class FactorAge:
             jmCrossingGap = random_noise().item()
         
         return jmCrossingGap
+    
+    def get_collisionMinGapFactor(self)->float:
+        collisionMinGapFactor = 0.0
+        if self.age < 33:
+            collisionMinGapFactor = distrs.Uniform(0.0, 1.0)().item()  # lower_bound = 0.0, upper_bound = 1.0
+        elif 33 <= self.age < 55:
+            collisionMinGapFactor = distrs.Normal(0.5, 0.1)().item()  # mean = 0.5, sigma = 0.1
+        else:
+            collisionMinGapFactor = distrs.Normal(0.7, 0.15)().item()  # mean = 0.7, sigma = 0.15
+        
+        return collisionMinGapFactor
+    
+    def get_minGapLat(self)->float:
+        minGapLat = 0.0
+        if self.age < 30:
+            minGapLat = distrs.Laplace(-0.2, 0.05)().item()  # mean = -0.2, sigma = 0.05
+        elif 30 <= self.age < 50:
+            minGapLat = distrs.Normal(0.0, 0.5)().item()  # mean = 0.0, sigma = 0.5
+        else:
+            minGapLat = distrs.Uniform(-0.2, 0.2)().item()  # lower_bound = -0.2, upper_bound = 0.2
+        
+        return minGapLat 
+    
+    def get_maxSpeedLat(self)->float:
+        maxSpeedLat = 0.0
+        if self.age < 23:
+            maxSpeedLat = distrs.Normal(0.2, 0.12)().item()  # mean = 0.2, sigma = 0.12
+        elif 23 <= self.age < 45:
+            maxSpeedLat = distrs.Normal(0.08, 0.15)().item()  # mean = 0.08, sigma = 0.15
+        else:
+            maxSpeedLat = distrs.Uniform(-0.1, 0.1)().item()  # lower_bound = -0.1, upper_bound = 0.1
+            
+        return maxSpeedLat
+    
+    def get_jmIgnoreFoeProb(self)->float:
+        jmIgnoreFoeProb = 0.0
+        if self.age < 23:
+            jmIgnoreFoeProb = distrs.Uniform(0.1, 0.6)().item()  # lower_bound = 0.1, upper_bound = 0.6
+        elif 23 <= self.age < 40:
+            jmIgnoreFoeProb = distrs.Laplace(0.2, 0.05)().item()  # mean = 0.2, sigma = 0.05
+        else:
+            jmIgnoreFoeProb = distrs.Normal(0.11, 0.03)().item()  # mean = 0.11, sigma = 0.03
+        
+        return jmIgnoreFoeProb
+    
+    def get_jmIgnoreFoeSpeed(self)->float:
+        jmIgnoreFoeSpeed = 0.0
+        jmIgnoreFoeSpeed = random_noise().item() + 0.2
+        
+        return jmIgnoreFoeSpeed
+    
+    def get_impatience(self)->float:
+        impatience = 0.0
+        if self.age < 22:
+            impatience = distrs.Normal(0.3, 0.05)().item()  # mean = 0.3, sigma = 0.05
+        elif 22 <= self.age <= 40:
+            impatience = distrs.Exponential(4)().item()  # mean = 0.25, sigma = 0.0625
+        else:
+            impatience = distrs.Normal(0.1, 0.1)().item()  # mean = 0.1, sigma = 0.1
+        
+        return impatience
+
+
+
+
+    # Pending solution
+    def get_jmIgnoreKeepClearTime(self)->float:
+        # default = -1.0
+        return -1.0
+    
+    def get_jmDriveAfterRedTime(self)->float:
+        # default = -1.0
+        return -1.0
+    
+    def get_jmDriveAfterYellowTime(self)->float:
+        # default = -1.0
+        return -1.0
+    
+    # not in vType
+    def get_jmTimegapMinor(self)->float:
+        pass
+    
+    def get_jmSigmaMinor(self)->self:
+        pass
+    
+    # Probably,will be dropped later
+    def get_laneChangeModel(self)->str:
+        pass
+
+    def get_latAlignment(self)->str:
+        pass
+
+    def get_carFollowingModel(self)->str:
+        pass
+    
+    # Dropped, but may be usefull
+    def get_speedFactor(self)->float:
+        speedFactor = 0.0
+        if 17 <= self.age < 21:
+            speedFactor = distrs.Laplace(0.2, 0.25)().item()  # mean = 0.2, sigma = 0.25
+        elif 21 <= self.age < 65:
+            speedFactor = distrs.Normal(0.0, 0.5)().item()  # mean = 0, sigma = 0.5
+        else:
+            condition = { 0: "Normal", 1: "Fast" }[distrs.Bernoulli(0.3)().item()]  # success_prob = 0.3
+            if condition == "Normal":
+                speedFactor = distrs.Normal(0.0, 0.2)().item()  # mean = 0, sigma = 0.2
+            else:
+                speedFactor = distrs.Uniform(0.25, 0.35)().item()  # lower_bound = 0.25, upper_bound = 0.35
