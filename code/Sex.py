9203eb2b30a95e28a4d3012831f331a0a4e661e9
Sergey Petrovich <serg.petrovich2000@gmail.com>
2020-06-14 14:58:41 +0300

    Update Sex.py

--- code/Sex.py
+++ code/Sex.py
@@ -1,20 +1,13 @@
 import pyro
 import pyro.distributions as distrs
-import random
-
 
 pyro.set_rng_seed(100)
 
 random_noise = distrs.Normal(0.0, 0.05) 
 
-# depends on age sooo much!!!!!!
-
 class FactorSex:
-    
     def __init__(self, sex: int):
         self.sex = sex
-        
-        
         
     def get_accel(self)->float:
         if self.sex == 0:
@@ -22,15 +15,11 @@ class FactorSex:
         elif self.sex == 1:
             return distrs.Normal(0.05, 0.17)().item()
         
-        
-        
     def get_decel(self)->float:
         if self.sex == 0:
             return distrs.Normal(0.15, 0.15)().item()
         elif self.sex == 1:
             return distrs.Normal(0.05, 0.07)().item()
-    
-    
     
     def get_sigma(self)->float:
         if self.sex == 0:
@@ -38,15 +27,11 @@ class FactorSex:
         elif self.sex == 1:
             return distrs.Normal(0.04, 0.11)().item() 
         
-        
-        
     def get_minGap(self)->float:
         if self.sex == 0:
             return distrs.Normal(0.0, 0.08)().item() 
         elif self.sex == 1:
             return distrs.Normal(-0.04, 0.08)().item() 
-    
-    
     
     def get_actionStepLength(self)->float:
         if self.sex == 0:
@@ -54,12 +39,8 @@ class FactorSex:
         elif self.sex == 1:
             return distrs.Normal(-0.1, 0.08)().item() 
         
-        
-        
     def get_jmCrossingGap(self)->float:
         if self.sex == 0:
             return distrs.Uniform(0.0, 0.05)().item()
         elif self.sex == 1:
-            return distrs.Normal(0.1, 0.1)().item()  
-        
-        
+            return distrs.Normal(0.1, 0.1)().item()
