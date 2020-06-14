32af709bd93422599d7fce282af590204fdd931b
Sergey Petrovich <serg.petrovich2000@gmail.com>
2020-06-14 15:02:32 +0300

    Update and rename DrivingUnderInfluence.py to Alcohol.py

--- code/Alcohol.py
+++ code/Alcohol.py
@@ -1,18 +1,13 @@
 import pyro
 import pyro.distributions as distrs
 
-
 pyro.set_rng_seed(100)
 
 random_noise = distrs.Normal(0.0, 0.05)  # mean = 0.0, sigma = 0.05
 
-
-class FactorDrivingUnderInfluence: #DrivingUnderInfluence - dui
-
+class FactorAlcohol:
     def __init__(self, dui: float):
         self.dui = dui
-    
-    
     
     def get_accel(self)->float:
         if self.dui < 0.15:
@@ -23,8 +18,6 @@ class FactorDrivingUnderInfluence: #DrivingUnderInfluence - dui
             return distrs.Exponential(3)().item()
         return 0
         
-        
-        
     def get_decel(self)->float:
         if self.dui < 0.15:
             return distrs.Normal(0.0, 0.1)().item() 
@@ -34,24 +27,18 @@ class FactorDrivingUnderInfluence: #DrivingUnderInfluence - dui
             return distrs.Exponential(3)().item()
         return 0
     
-    
-    
     def get_sigma(self)->float:
         if self.dui < 0.5:
             return distrs.Normal(0.0, 0.1)().item() 
         else:
             return distrs.Normal(0.2, 0.17)().item() 
         return 0
-        
-        
-        
+            
     def get_minGap(self)->float:
         if self.dui < 0.3:
             return distrs.Normal(0.0, 0.15)().item()    
         else:
             return distrs.Normal(-0.1, 0.07)().item() 
-    
-    
     
     def get_actionStepLength(self)->float:
         if self.dui == 0:
@@ -63,12 +50,8 @@ class FactorDrivingUnderInfluence: #DrivingUnderInfluence - dui
         elif self.dui < 0.5:
             return distrs.Normal(0.15, 0.1)().item()    
         else:
-            return distrs.Normal(0.2, 0.11)().item()    
-        
-        
+            return distrs.Normal(0.2, 0.11)().item()          
         
     def get_jmCrossingGap(self)->float:
         return distrs.Normal(0.0, 0.05)().item()
         
-        
-    
