e319f3f8370918ce4b0977315fb124676528df80
Timur Vankov <timur.vankov@gmail.com>
2020-06-19 11:43:19 +0300

    bash refactored

--- make.sh
+++ make.sh
@@ -6,8 +6,18 @@ cp -r webWizard ../
 cp randomTrips.py ../
 
 cd ..
-number=$RANDOM%10
+
 default_port=8080
-port=$(($default_port+$number))
-echo port $port
-python3 osmWebWizard.py --port $port
+
+if [[ $1 == 2 ]]
+then
+    number=$RANDOM%10
+    default_port=8080
+    port=$(($default_port+$number+1))
+    echo port1 $default_port
+    echo port2 $port
+    python3 osmWebWizard.py | python3 osmWebWizard.py --port $port
+else
+    echo port $default_port
+    python3 osmWebWizard.py
+fi
